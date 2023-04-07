785f8cd7cc190ec9e1167c22bd01f4a5ebc7d5cb
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-04-07 15:05:56 +0300

    Add ships objects

--- js/battleship.js
+++ js/battleship.js
@@ -14,3 +14,28 @@ let view = {
         miss.setAttribute('class', 'miss');
     }
 };
+
+view.displayMiss('00');
+view.displayHit('34');
+view.displayMiss('55');
+view.displayHit('12');
+view.displayMiss('25');
+view.displayHit('26');
+
+view.displayMessage('Tap tap, is this thing on?');
+
+let ship1 = {
+    locations: ['10','20','30'],
+    hits: ['','','']
+};
+
+let ship2 = {
+    locations: ['32','33','34'],
+    hits: ['','','']
+};
+
+let ship3 = {
+    locations: ['63','64','65'],
+    hits: ['','','hit']
+};
+
