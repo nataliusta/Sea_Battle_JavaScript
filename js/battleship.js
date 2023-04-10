8265bcd32fd25a457d26a0df15fcc3b7637801cc
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-04-10 21:27:47 +0300

    Update processGuess

--- js/battleship.js
+++ js/battleship.js
@@ -84,7 +84,14 @@ let controller = {
     guesses: 0,
 
     processGuess: function(guess) {
-
+        let location = parseGuess(guess);
+        if (location) {
+            this.guesses++;
+            let hit = model.fire(location);
+            if ( hit && model.shipsSunk === model.numShips) {
+                view.displayMessage('You sank all my battleships, in ' + this.guesses + ' guesses');
+            }
+        }
     }
 };
 
@@ -109,3 +116,9 @@ function parseGuess(guess) {
     }
     return null;
 }
+
+console.log(parseGuess('A0'));
+console.log(parseGuess('B6'));
+console.log(parseGuess('G3'));
+console.log(parseGuess('H0'));
+console.log(parseGuess('A7'));
