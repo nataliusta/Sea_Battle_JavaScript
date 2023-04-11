ce502b2da71d9d310cc287c19f2609a4baee2763
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-04-11 17:57:14 +0300

    Add handleFireButton function

--- js/battleship.js
+++ js/battleship.js
@@ -68,18 +68,6 @@ let model = {
     }
 };
 
-model.fire('53');
-model.fire('06');
-model.fire('16');
-model.fire('26');
-model.fire('34');
-model.fire('24');
-model.fire('44');
-model.fire('66');
-model.fire('11');
-model.fire('12');
-model.fire('15');
-
 let controller = {
     guesses: 0,
 
@@ -89,7 +77,7 @@ let controller = {
             this.guesses++;
             let hit = model.fire(location);
             if ( hit && model.shipsSunk === model.numShips) {
-                view.displayMessage('You sank all my battleships, in ' + this.guesses + ' guesses');
+                view.displayMessage('You sank all my battleships in ' + this.guesses + ' guesses');
             }
         }
     }
@@ -117,8 +105,17 @@ function parseGuess(guess) {
     return null;
 }
 
-console.log(parseGuess('A0'));
-console.log(parseGuess('B6'));
-console.log(parseGuess('G3'));
-console.log(parseGuess('H0'));
-console.log(parseGuess('A7'));
+controller.processGuess('A0');
+controller.processGuess('A6');
+controller.processGuess('B6');
+controller.processGuess('C6');
+controller.processGuess('C4');
+controller.processGuess('D4');
+controller.processGuess('E4');
+controller.processGuess('B0');
+controller.processGuess('B1');
+controller.processGuess('B2');
+
+function handleFireButton() {
+    
+}
